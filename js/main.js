87c446d378b49ee5c57fa6b49c669774761cc83e
jakimfett <jakimfett@gmail.com>
2015-11-14 20:55:54 -0800

    Attempting to load multiple files

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ template.open('GET', 'content/TEMPLATE.md');
 
 template.onreadystatechange = function() {
   var placeholder = template.responseText.replace(/\r?\n/g,'<br/>');
-  var text = placeholder.replace("de063c99357e6675f1ba05b33635e044", "Title goes here");
-  document.getElementById("content").innerHTML = text;
+
+  var blocks = new XMLHttpRequest();
+  blocks.open('GET', 'content/BLOCKS.md');
+  blocks.onreadystatechange = function() {
+    var titleMD5 = "de063c99357e6675f1ba05b33635e044";
+
+    var blocksArray = blocks.responseText.split(/\r?\n/g);
+    
+    var titleIndex = blocksArray.indexOf(titleMD5);
+    var title = blocks[titleIndex];
+    var text = placeholder.replace(titleMD5, "test");
+    document.getElementById("content").innerHTML = text;
+  }
 }
 template.send();
