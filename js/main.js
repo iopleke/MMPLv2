1e5e5297ada64f6fde5719bb521383715691e61a
jakimfett <jakimfett@gmail.com>
2015-11-18 10:52:07 -0800

    Functionify javascript

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
-var template = new XMLHttpRequest();
-template.open('GET', 'content/TEMPLATE.md');
+function loadJSON(file, callback) {
+    console.log("Loading file: " + file);
 
-template.onreadystatechange = function() {
-  var placeholder = template.responseText.replace(/\r?\n/g,'<br/>');
-
-  var blocks = new XMLHttpRequest();
-  blocks.open('GET', 'content/BLOCKS.md');
-  blocks.onreadystatechange = function() {
-    var titleMD5 = "de063c99357e6675f1ba05b33635e044";
-
-    var blocksArray = blocks.responseText.split(/\r?\n/g);
-    
-    var titleIndex = blocksArray.indexOf(titleMD5);
-    var title = blocks[titleIndex];
-    var text = placeholder.replace(titleMD5, "test");
-    document.getElementById("content").innerHTML = text;
-  }
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState === 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value 
+            // but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
-template.send();
+
+function loadData() {
+
+    console.log("Loading data from json files...");
+
+    var template = "";
+
+    loadJSON("content/template.json", function (response) {
+        var parsedResponse = JSON.parse(response);
+        for (var key in parsedResponse) {
+            if (parsedResponse.hasOwnProperty(key)) {
+                template += key[0].toUpperCase() + key.slice(1);
+                template += "<br/>";
+                template += parsedResponse[key];
+                template += "<br/>";
+
+            }
+        }
+        console.log("Writing data to page...");
+        document.getElementById("content").innerHTML = template;
+        console.log("Done writing data to page.");
+
+    });
+
+    console.log("Done loading data.");
+
+
+    console.log("All done.");
+}
+
+window.load = loadData();
