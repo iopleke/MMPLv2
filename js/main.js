5eb72cb847ae7464a37cfabf6ab26745c9d739c6
jakimfett <jakimfett@gmail.com>
2015-11-18 11:13:24 -0800

    Replace block section from data file

--- js/main.js
+++ js/main.js
@@ -16,25 +16,41 @@ function loadJSON(file, callback) {
 
 function loadData() {
 
+    var titleMD5 = "de063c99357e6675f1ba05b33635e044";
+
     console.log("Loading data from json files...");
 
-    var template = "";
+    document.getElementById("content").innerHTML = "";
 
     loadJSON("content/template.json", function (response) {
         var parsedResponse = JSON.parse(response);
         for (var key in parsedResponse) {
             if (parsedResponse.hasOwnProperty(key)) {
-                template += key[0].toUpperCase() + key.slice(1);
-                template += "<br/>";
-                template += parsedResponse[key];
-                template += "<br/>";
+                document.getElementById("content").innerHTML += parsedResponse[key];
+                document.getElementById("content").innerHTML += "<br/>";
 
             }
         }
-        console.log("Writing data to page...");
-        document.getElementById("content").innerHTML = template;
-        console.log("Done writing data to page.");
 
+        loadJSON("content/heading.json", function (response) {
+            var parsedResponse = JSON.parse(response);
+            for (var key in parsedResponse) {
+                if (parsedResponse.hasOwnProperty(key)) {
+                    document.getElementById("content").innerHTML = document.getElementById("content").innerHTML.replace(key, parsedResponse[key]);
+                }
+            }
+        });
+
+        loadJSON("content/data.json", function (response) {
+            var parsedResponse = JSON.parse(response);
+            for (var key in parsedResponse) {
+                if (parsedResponse.hasOwnProperty(key)) {
+                    if (parsedResponse[key].hasOwnProperty("value")) {
+                        document.getElementById("content").innerHTML = document.getElementById("content").innerHTML.replace(key, parsedResponse[key]["value"]);
+                    }
+                }
+            }
+        });
     });
 
     console.log("Done loading data.");
